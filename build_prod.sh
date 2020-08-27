ed5d9acab427494be2b921b01b431419ea8d1acd
fourtf <tf.four@gmail.com>
2020-08-27 22:28:46 +0200

    maybe?

--- build_prod.sh
+++ build_prod.sh
@@ -4,13 +4,13 @@ set -e
 
 srcfile="src/Main.elm"
 js="tmp.js"
-min="prod/app.min.js"
+min="./prod/app.min.js"
 
 elm make --optimize --output=$js $srcfile || elm.exe make --optimize --output=$js $srcfile
 
 uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > $min
 
-rm tmp.js
+rm $js
 
 echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
 echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
